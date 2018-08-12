d05c661b18f41915776d33a008499ec13961fb64
Philip Mayer <shadowsith94@gmail.com>
2018-08-12 15:32:03 +0200

    add new blank plugin class files

---
+++ plugins/delete.rb
@@ -0,0 +1,30 @@
+class Delete < Plugin
+
+    def init(init)
+        super
+        logger("INFO: INIT plugin #{self.class.name}.")
+        @@bot[:bot] = self
+        return @@bot
+    end
+
+    def name
+        self.class.name
+    end
+
+    def help(h)
+        h << "<hr><span style='color:red;'>Plugin #{self.class.name}</span><br>"
+        h << "<b>#{Conf.gvalue("main:control:string")}delete [file] - delete file from bot music folder<br>"
+        h
+    end
+
+    def handle_chat(msg, message) 
+        super
+        msgParts = message.split(" ")
+        if msgParts[0] == "delete"
+            if !msgParts[1].to_s.empty? 
+                file = msgParts[1]
+            end
+        end
+    end
+end
+
---
+++ plugins/metadata.rb
@@ -0,0 +1,27 @@
+class Metadata < Plugin
+
+    def init(init)
+        super
+        logger("INFO: INIT plugin #{self.class.name}.")
+        @@bot[:bot] = self
+        return @@bot
+    end
+
+    def name
+        self.class.name
+    end
+
+    def help(h)
+        h << "<hr><span style='color:red;'>Plugin #{self.class.name}</span><br>"
+        h << "<b>#{Conf.gvalue("main:control:string")}meta [title|artist] [value] [file] - add/change metadata from audiofiles<br>"
+        h << "<b>#{Conf.gvalue("main:control:string")}meta [file] - show metadata from audiofiles<br>"
+        h
+    end
+
+    def handle_chat(msg, message) 
+        super
+        if message == "meta"
+        end
+    end
+end
+
