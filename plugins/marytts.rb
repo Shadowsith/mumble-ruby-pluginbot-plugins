cb84d9260906e8bf8149ca92dfa9aa0b607845f6
Philip Mayer <shadowsith94@gmail.com>
2018-08-16 00:00:55 +0200

    moved methods to private, added some new methods to avoid bad code on hande_chat

--- plugins/marytts.rb
+++ plugins/marytts.rb
@@ -2,83 +2,8 @@ require "yaml"
 require "../helpers/maryttsh.rb"
 
 class MaryTTS < Plugin
-    private
+  private
     CONFIG = "../plugins/marytts.yml"
-
-    public
-    def init(init)
-        super
-        logger("INFO: INIT plugin #{self.class.name}.")
-        @@bot[:mtts] = self
-        return @@bot
-    end
-
-    def name
-        self.class.name
-    end
-
-    def help(h)
-        h << "<hr><span style='color:red;'>Plugin #{self.class.name}</span><br>"
-        h << "<b>#{Conf.gvalue("main:control:string")}say [message]</b> - bot says the message<br>"
-        h << "<b>#{Conf.gvalue("main:control:string")}mlang [de|en]</b> - change language of the bot<br>"
-        h << "<b>#{Conf.gvalue("main:control:string")}mvoice [male|female]</b> - change voice of the bot<br>"
-        h << "<b>#{Conf.gvalue("main:control:string")}msize [number]</b> - allowed number of characters (max 200)<br>"
-        h << "<b>#{Conf.gvalue("main:control:string")}mconf - get settings<br>"
-        h
-    end
-
-    def handle_chat(msg, message)
-        super
-        parts = message.split(" ")
-        begin
-        if parts[0] == "say"
-            if parts[1] != "" || parts[1] != nil?
-                message = message.to_s.sub("say","").gsub("<br>"," ")
-                if message.length <= getSize.to_i
-                    lang = getLang
-                    if lang.to_s.empty?
-                        lang = "en_US"
-                    end
-                    voice = getVoice
-                    if voice.to_s.empty?
-                        voice = "male"
-                    end
-                    path = Conf.gvalue("plugin:mpd:musicfolder")
-                    mary = MaryTTSHelper.new(path, message, lang, voice)
-                    mary.load
-                    @@bot[:mpd].update
-                    @@bot[:mpd].add(mary.getFileName)
-                    if @@bot[:mpd].queue.length > 0
-                        lastsongid = @@bot[:mpd].queue.length.to_i - 1
-                        @@bot[:mpd].play (lastsongid)
-                        @@bot[:cli].me.deafen false if @@bot[:cli].me.deafened?
-                        @@bot[:cli].me.mute false if @@bot[:cli].me.muted?
-                        clearQueue(mary)
-                    end
-                else 
-                    privatemessage("The message is greater than the allowed limit of "+getSize.to_s+" characters!")
-                end
-            end
-        end
-        if parts[0] == "mlang"
-            setLang(parts[1])
-        end
-        if parts[0] == "mvoice"
-            setVoice(parts[1])
-        end
-        if parts[0] == "msize"
-            setSize(parts[1])
-        end
-        if parts[0] == "mconf"
-            messageto(msg.actor,"<br>Language: "+getLang+"<br>Voice: "+getVoice+"<br>Max Characters: "+getSize.to_s+"<br>")
-        end
-        if parts[0] == "fconf"
-            privatemessage(@@bot[:mpd].config.instance_variables.to_s)
-        end
-        rescue Exception => ex
-            messageto(msg.actor,"MarryTTS Error: "+ex.message)
-        end
-    end
 
     def getLang
         data = YAML::load(File.open(CONFIG))
@@ -130,6 +55,24 @@ class MaryTTS < Plugin
         end
     end
 
+    def updateBot
+        @@bot[:mpd].update
+        while @@bot[:mpd].status[:updating_db] do
+            sleep 0.5
+        end
+    end
+
+    def play(mary)
+        @@bot[:mpd].add(mary.getFileName)
+        if @@bot[:mpd].queue.length > 0
+            lastsongid = @@bot[:mpd].queue.length.to_i - 1
+            @@bot[:mpd].play (lastsongid)
+            @@bot[:cli].me.deafen false if @@bot[:cli].me.deafened?
+            @@bot[:cli].me.mute false if @@bot[:cli].me.muted?
+            clearQueue(mary)
+        end
+    end
+
     def clearQueue(mary)
         songnr = 0
         @@bot[:mpd].queue.each do |song|
@@ -142,4 +85,73 @@ class MaryTTS < Plugin
             songnr+=1
         end
     end
+
+  public
+    def init(init)
+        super
+        logger("INFO: INIT plugin #{self.class.name}.")
+        @@bot[:mtts] = self
+        return @@bot
+    end
+
+    def name
+        self.class.name
+    end
+
+    def help(h)
+        h << "<hr><span style='color:red;'>Plugin #{self.class.name}</span><br>"
+        h << "<b>#{Conf.gvalue("main:control:string")}say [message]</b> - bot says the message<br>"
+        h << "<b>#{Conf.gvalue("main:control:string")}mlang [de|en]</b> - change language of the bot<br>"
+        h << "<b>#{Conf.gvalue("main:control:string")}mvoice [male|female]</b> - change voice of the bot<br>"
+        h << "<b>#{Conf.gvalue("main:control:string")}msize [number]</b> - allowed number of characters (max 200)<br>"
+        h << "<b>#{Conf.gvalue("main:control:string")}mconf - get settings<br>"
+        h
+    end
+
+    def handle_chat(msg, message)
+        super
+        parts = message.split(" ")
+        begin
+        if parts[0] == "say"
+            if parts[1] != "" || parts[1] != nil?
+                message = message.to_s.sub("say","").gsub("<br>"," ")
+                if message.length <= getSize.to_i
+                    lang = getLang
+                    if lang.to_s.empty?
+                        lang = "en_US"
+                    end
+                    voice = getVoice
+                    if voice.to_s.empty?
+                        voice = "male"
+                    end
+                    path = Conf.gvalue("plugin:mpd:musicfolder")
+                    mary = MaryTTSHelper.new(path, message, lang, voice)
+                    mary.load
+                    updateBot
+                    play(mary)
+                else 
+                    privatemessage("The message is greater than the allowed limit of "+getSize.to_s+" characters!")
+                end
+            end
+        end
+        if parts[0] == "mlang"
+            setLang(parts[1])
+        end
+        if parts[0] == "mvoice"
+            setVoice(parts[1])
+        end
+        if parts[0] == "msize"
+            setSize(parts[1])
+        end
+        if parts[0] == "mconf"
+            messageto(msg.actor,"<br>Language: "+getLang+"<br>Voice: "+getVoice+"<br>Max Characters: "+getSize.to_s+"<br>")
+        end
+        if parts[0] == "fconf"
+            privatemessage(@@bot[:mpd].config.instance_variables.to_s)
+        end
+        rescue Exception => ex
+            messageto(msg.actor,"MarryTTS Error: "+ex.message)
+        end
+    end
+
 end
