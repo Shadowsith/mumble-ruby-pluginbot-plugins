3f390fa7706a60b2694002f9bec1d10820414ee3
Philip Mayer <shadowsith94@gmail.com>
2018-08-16 10:26:33 +0200

    prettified ruby code with rufo

--- plugins/splay.rb
+++ plugins/splay.rb
@@ -1,50 +1,48 @@
 class SearchPlay < Plugin
+  def init(init)
+    super
+    logger("INFO: INIT plugin #{self.class.name}.")
+    @@bot[:bot] = self
+    return @@bot
+  end
 
-    def init(init)
-        super
-        logger("INFO: INIT plugin #{self.class.name}.")
-        @@bot[:bot] = self
-        return @@bot
-    end
+  def name
+    self.class.name
+  end
 
-    def name
-        self.class.name
-    end
+  def help(h)
+    h << "<hr><span style='color:red;'>Plugin #{self.class.name}</span><br>"
+    h << "<b>#{Conf.gvalue("main:control:string")}splay [file]</b> - add first match to playlist and plays it<br>"
+    h << "<b>#{Conf.gvalue("main:control:string")}spl</b> - alias for splay<br>"
+    h
+  end
 
-    def help(h)
-        h << "<hr><span style='color:red;'>Plugin #{self.class.name}</span><br>"
-        h << "<b>#{Conf.gvalue("main:control:string")}splay [file]</b> - add first match to playlist and plays it<br>"
-        h << "<b>#{Conf.gvalue("main:control:string")}spl</b> - alias for splay<br>"
-        h
-    end
-
-    def handle_chat(msg, message) 
-        super
-        parts = message.split(" ")
-        if parts[0] == "splay" || parts[0] == "spl"
-            if !parts[1].to_s.empty?
-                text_out ="#{I18n.t("plugin_mpd.add.added")}<br/>"
-                count = 0
-                @@bot[:mpd].where(any: "#{parts[1]}").each do |song|
-                    text_out << "add #{song.file}<br/>"
-                    @@bot[:mpd].add(song)
-                    count += 1
-                    break
-                end
-                if count == 0
-                    text_out = I18n.t("plugin_mpd.add.nothing")
-                else
-                    lastsongid = @@bot[:mpd].queue.length.to_i - 1
-                    @@bot[:mpd].play lastsongid
-                    @@bot[:cli].me.deafen false if @@bot[:cli].me.deafened?
-                    @@bot[:cli].me.mute false if @@bot[:cli].me.muted?
-                    privatemessage(I18n.t("plugin_mpd.play.last", :id => lastsongid))
-                end
-            else
-                text_out = I18n.t("plugin_mpd.add.empty")
-            end
+  def handle_chat(msg, message)
+    super
+    parts = message.split(" ")
+    if parts[0] == "splay" || parts[0] == "spl"
+      if !parts[1].to_s.empty?
+        text_out = "#{I18n.t("plugin_mpd.add.added")}<br/>"
+        count = 0
+        @@bot[:mpd].where(any: "#{parts[1]}").each do |song|
+          text_out << "add #{song.file}<br/>"
+          @@bot[:mpd].add(song)
+          count += 1
+          break
         end
-        privatemessage(text_out)
+        if count == 0
+          text_out = I18n.t("plugin_mpd.add.nothing")
+        else
+          lastsongid = @@bot[:mpd].queue.length.to_i - 1
+          @@bot[:mpd].play lastsongid
+          @@bot[:cli].me.deafen false if @@bot[:cli].me.deafened?
+          @@bot[:cli].me.mute false if @@bot[:cli].me.muted?
+          privatemessage(I18n.t("plugin_mpd.play.last", :id => lastsongid))
+        end
+      else
+        text_out = I18n.t("plugin_mpd.add.empty")
+      end
     end
+    privatemessage(text_out)
+  end
 end
-
--- plugins/system.rb
+++ plugins/system.rb
@@ -2,32 +2,30 @@
 # please use this plugin only if you have a secure system configuration
 
 class System < Plugin
+  def init(init)
+    super
+    logger("INFO: INIT plugin #{self.class.name}.")
+    @@bot[:bot] = self
+    return @@bot
+  end
 
-    def init(init)
-        super
-        logger("INFO: INIT plugin #{self.class.name}.")
-        @@bot[:bot] = self
-        return @@bot
-    end
+  def name
+    self.class.name
+  end
 
-    def name
-        self.class.name
-    end
+  def help(h)
+    h << "<hr><span style='color:red;'>Plugin #{self.class.name}</span><br>"
+    h << "<b>#{Conf.gvalue("main:control:string")}sysinfo</b> - information about operating system and mumble-server<br>"
+    h
+  end
 
-    def help(h)
-        h << "<hr><span style='color:red;'>Plugin #{self.class.name}</span><br>"
-        h << "<b>#{Conf.gvalue("main:control:string")}sysinfo</b> - information about operating system and mumble-server<br>"
-        h
+  def handle_chat(msg, message)
+    super
+    actor = msg.actor
+    if message == "sysinfo"
+      head = "<br><span style='color:lightblue;'>System information:</span><br>"
+      os = %x( uname -smr ) + "<br>"
+      messageto(actor, head + os)
     end
-
-    def handle_chat(msg, message) 
-        super
-        actor = msg.actor
-        if message == "sysinfo"
-            head = "<br><span style='color:lightblue;'>System information:</span><br>"
-            os = %x( uname -smr ) + "<br>"
-            messageto(actor, head+os)
-        end
-    end
+  end
 end
-
